a3febcc2e2c3a7dd7eb1aebd42bde09293f5fe03
Aaron Gibson <aaron@goreact.com>
2016-12-21 22:11:18 -0700

    addming manifest to clone repo and run nginx on port 8000

--- challange.pp
+++ challange.pp
@@ -1,11 +1,11 @@
-# let's setup the repo
+# let's setup the repo, provided from puppetlabs
 vcsrepo { '/var/www/www.puppetlabs.com':
   ensure   => present,
   provider => git,
   source   => 'https://github.com/puppetlabs/exercise-webpage.git',
 }
 
-# now the nginx webserver
+# now the nginx webserver, on port 8000
 class{'nginx': }
   nginx::resource::vhost { 'www.puppetlabs.com':
   listen_port => 8000,
